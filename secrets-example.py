0fc97ab94863a109336a9132def2a88543d9fcdf
Russell Fisher <russell_fisher@Russells-MacBook-Pro.local>
2019-04-05 20:33:44 -0600

    Add example secret file

---
+++ secrets-example.py
@@ -0,0 +1,8 @@
+secrets = {
+    'ssid' : 'WIFI-network',
+    'password' : 'WIFI-password',
+    'timezone' : "America/Denver",
+    'openweather_token' : 'openweather_token',
+    'aio_username' : 'username',
+    'aio_key' : 'key'
+    }
